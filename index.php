142bc0a33be758d36cc8729f11b7973a79efdfa5
Haseeb-Qamar <41240895+Haseeb-Qamar@users.noreply.github.com>
2018-07-27 23:06:15 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="bs/css/bootstrap.css">
+    <link rel="stylesheet" href="stylesheet/theme.css">
+    <script type="text/javascript" src="bs/js/bootstrap.js">
+
+    </script>
+    <title>Login</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-default justify-content-between" style="background-color:rgba(0,0,0,.9);color:white;">
+  <a class="navbar-brand">E-WEB</a>
+  <form class="form-inline">
+    <input type="text" name="" value="" placeholder="UserName">
+    <input type="password" name="" value="" placeholder="Password">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+  </form>
+</nav>
+    <div class="container">
+      <div class="row">
+        <div class="signup">
+          <p class="text-center">Sign-Up</p>
+          <div class="inputs">
+
+            <input type="text" name="fn" value="" placeholder="First Name">
+
+            <input type="text" name="ln" value="" placeholder="Last Name">
+
+            <input type="password" name="" value="" placeholder="Password">
+
+            <input type="email" name="email" value="" placeholder="Email">
+            <p class="text-center"><input id="btn0" type="submit" name="" value="Sign-Up" class="btn btn-outline-light btn-sm"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
